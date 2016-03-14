6dc67d282baacad54db53371f43da58ff11c9ea5
Konstantinos Zacharis <s1569729@sms.ed.ac.uk>
2016-03-14 19:49:44 +0000

    Added files via upload

---
+++ createSpatialIndex.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS ItemLocation (ItemID varchar(50) NOT NULL,
+						   Location POINT NOT NULL,
+						   SPATIAL INDEX(Location),
+						   PRIMARY KEY (ItemID, Location),
+						   FOREIGN KEY (ItemID) REFERENCES item_coordinates(ItemID)
+						   ) ENGINE=MyISAM;
+
+INSERT INTO ItemLocation (ItemID, Location)
+SELECT item_id, POINT(latitude, longitude) 
+FROM item_coordinates
+WHERE latitude IS NOT NULL AND longitude IS NOT NULL;
